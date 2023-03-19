d0b9780a20af60aab5963d494a17a95f71076679
Vitor <vcsoouza@hotmail.com>
2023-03-19 21:39:41 +0200

    Initial commit

---
+++ src/GameFrame.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+    GamePanel panel;
+    GameFrame(){
+        panel  = new GamePanel();
+        this.add(panel);
+        this.setTitle("Ping Pong Game");
+        this.setResizable(false);
+        this.setBackground(new Color(34,45,94));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
